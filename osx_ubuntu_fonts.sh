df7a83c325d24ff29d23762f7aa45febfe4139a2
İsmail SEZEN <sezenismail@gmail.com>
2016-03-13 16:35:46 +0200

    sh fix and tmp folder support

--- osx_ubuntu_fonts.sh
+++ osx_ubuntu_fonts.sh
@@ -50,17 +50,20 @@ if [ -f  "$dir_fonts/Ubuntu-M.ttf" ]; then
 else
   # get latest Ubuntu Fonts download url
   # shellcheck disable=SC1003
-  url=$(grep -e 'zip' <(curl -s "http://font.ubuntu.com")|
-        sed 's/\"\.\./\'$'\n\"\.\./g' | sed 's/\"/\"\'$'\n/2'|grep "\"../")
+  url=$(curl -s "http://font.ubuntu.com"|
+        grep -e 'zip'|
+        sed 's/\"\.\./\'$'\n\"\.\./g'|
+        sed 's/\"/\"\'$'\n/2'|grep "\"../")
   url="${url//\"}" # remove " symbols, curl complains
   url=$(echo "$url" | sed -e 's/\.\./http:\/\/font.ubuntu.com/g')
   fname=${url##*/} # get filename
+  fname_tmp="/tmp/$fname"
   url=$( printf "%s\n" "$url" | sed 's/ /%20/g') # replace space by %20
-  if [ ! -f "$fname" ]; then # if zip file does not exist
+  if [ ! -f "$fname_tmp" ]; then # if zip file does not exist
   	echo "* Downloading: $fname"
-    curl -s -o "$fname" "$url"
+    curl -s -o "$fname_tmp" "$url"
   fi
-  unzip -qoj "$fname" /*.ttf -d "$dir_fonts/"
-  rm "$fname"
+  # shellcheck disable=SC2035
+  unzip -qoj "$fname_tmp" *.ttf -d "$dir_fonts/"
   echo "* Installed: Ubuntu Fonts"
 fi
